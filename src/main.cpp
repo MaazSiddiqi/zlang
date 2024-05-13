6562c714363d3b37b947371788dbe95e277cb83c
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-05-12 21:33:44 -0400

    moved grammer to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,52 @@
 #include <iostream>
 
 #include "lexer.h"
+#include "parser.h"
 #include "scanner.h"
 
 using namespace std;
+
+/*
+ * === Grammer ===
+ * program                -> fn main() scope EOF
+ * scope                  -> {stmts}
+ * stmts                  -> stmt stmts
+ * stmts                  -> ϵ
+ * stmt                   -> func_decl
+ * stmt                   -> scope
+ * stmt                   -> while_stmt
+ * stmt                   -> if_stmt
+ * stmt                   -> expr;
+ * stmt                   -> ϵ
+ * func_decl              -> fn id(args) scope
+ * while_stmt             -> while (conditions) scope
+ * if_stmt                -> if (conditions) scope if_tail
+ * if_tail                -> elsif (conditions) scope if_tail
+ * if_tail                -> else scope
+ * if_tail                -> ϵ
+ * expr                   -> id = expr
+ * expr                   -> return expr;
+ * expr                   -> add_expr
+ * expr                   -> ϵ
+ * add_expr               -> mult_expr '+' add_expr
+ * add_expr               -> mult_expr '-' add_expr
+ * add_expr               -> mult_expr
+ * mult_expr              -> term_expr '*' mult_expr
+ * mult_expr              -> term_expr '/' mult_expr
+ * term_expr              -> number
+ * term_expr              -> id
+ * term_expr              -> (expr)
+ * conditions             -> condition conditions_tail
+ * conditions_tail        -> lop conditions
+ * conditions_tail        -> ϵ
+ * condition              -> expr cmp expr
+ * args                   -> id args_tail
+ * args_tail              -> , id args_tail
+ * args_tail              -> ϵ
+ * op                     -> '+' | '-' | '*' | '/'
+ * lop                    -> '||' | '&&'
+ * cmp                    -> '<' | '>' | '<=' | '>=' | '==' | '!='
+ */
 
 int main() {
   ifstream file("../scripts/sample.z");
@@ -19,12 +62,15 @@ int main() {
   Lexer lexer(file);
   Scanner scanner(lexer);
 
-  // print lex report
-  while (!scanner.atEnd()) {
-    Token t = scanner.consume();
-    std::printf("%s: \'%.*s\'\n", token_type_name(t.type).c_str(), t.len,
-                t.lexeme);
-  }
+  // // print lex report
+  // while (!scanner.atEnd()) {
+  //   Token t = scanner.consume();
+  //   std::printf("\'%.*s\': %s\n", t.len, t.lexeme,
+  //               token_type_name(t.type).c_str());
+  // }
+  //
+  Parser parser(scanner);
+  parser.parse();
 
   // Close the file
   file.close();
--- src/parser.h
+++ src/parser.h
@@ -1,55 +1,26 @@
-/*
- * === Grammer ===
- * program                -> fn main() scope EOF
- * scope                  -> {stmts}
- * stmts                  -> stmt stmts
- * stmts                  -> ϵ
- * stmt                   -> func_decl
- * stmt                   -> scope
- * stmt                   -> while_stmt
- * stmt                   -> if_stmt
- * stmt                   -> expr;
- * stmt                   -> ϵ
- * func_decl              -> fn id(args) scope
- * while_stmt             -> while (conditions) scope
- * if_stmt                -> if (conditions) scope if_tail
- * if_tail                -> elsif (conditions) scope if_tail
- * if_tail                -> else scope
- * if_tail                -> ϵ
- * expr                   -> id = expr
- * expr                   -> return expr;
- * expr                   -> add_expr
- * expr                   -> ϵ
- * add_expr               -> mult_expr '+' add_expr
- * add_expr               -> mult_expr '-' add_expr
- * add_expr               -> mult_expr
- * mult_expr              -> term_expr '*' mult_expr
- * mult_expr              -> term_expr '/' mult_expr
- * term_expr              -> number
- * term_expr              -> id
- * term_expr              -> (expr)
- * conditions             -> condition conditions_tail
- * conditions_tail        -> lop conditions
- * conditions_tail        -> ϵ
- * condition              -> expr cmp expr
- * args                   -> id args_tail
- * args_tail              -> , id args_tail
- * args_tail              -> ϵ
- * op                     -> '+' | '-' | '*' | '/'
- * lop                    -> '||' | '&&'
- * cmp                    -> '<' | '>' | '<=' | '>=' | '==' | '!='
- */
+#ifndef PARSER_H
+#define PARSER_H
 
+#include "node.h"
 #include "scanner.h"
 
-struct Node_Prg {};
+struct Node_Stmts {};
+struct Node_Prg {
+  Node_Stmts stmts;
+};
 
 class Parser {
 public:
   Parser(Scanner &scan);
   ~Parser();
 
+  Node parse();
+
 private:
   Scanner &scan;
   Node_Prg parseProgram();
+  Node_Stmts parseStmts();
+  void throwUnexpected(Token &given, std::string expected);
 };
+
+#endif // PARSER_H
