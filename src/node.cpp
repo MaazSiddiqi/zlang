3986307d51c374da917af232b517692de1864b21
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-05-13 22:52:44 -0400

    added node ostream

--- src/node.cpp
+++ src/node.cpp
@@ -1,5 +1,25 @@
 #include "node.h"
+#include "token.h"
+#include <variant>
 
 Node::Node() {}
 
 Node::~Node() {}
+
+void Node::addToken(Token t) { children.push_back(t); }
+void Node::addNode(Node n) { children.push_back(n); }
+
+std::ostream &operator<<(std::ostream &os, const Node &node) {
+  for (auto &el : node.children) {
+    if (std::holds_alternative<Token>(el)) {
+      Token t = std::get<Token>(el);
+      os << token_lexeme(t) << std::endl;
+
+    } else if (std::holds_alternative<Node>(el)) {
+      Node n = std::get<Node>(el);
+      os << n;
+    }
+  }
+
+  return os;
+}
