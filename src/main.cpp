ddd2374be2beb55b0ab2b8b6333603f9a7821a96
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-05-21 01:34:57 -0400

    add conditions test

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ using namespace std;
  */
 
 int main() {
-  ifstream file("../scripts/func.z");
+  ifstream file("scripts/conditions.z");
 
   // Check if the file was opened successfully
   if (!file) {
@@ -64,18 +64,18 @@ int main() {
   Lexer lexer(file);
   Scanner scanner(lexer);
 
-  // // print lex report
-  // while (!scanner.atEnd()) {
-  //   Token t = scanner.consume();
-  //   std::printf("\'%.*s\': %s\n", t.len, t.lexeme,
-  //               token_type_name(t.type).c_str());
-  // }
+  // print lex report
+  while (!scanner.atEnd()) {
+    Token t = scanner.consume();
+    std::printf("\'%.*s\': %s\n", t.len, t.lexeme,
+                token_type_name(t.type).c_str());
+  }
+
+  // Parser parser(scanner);
+  // Node prg = parser.parse();
   //
-  Parser parser(scanner);
-  Node prg = parser.parse();
-
-  std::cout << prg << std::endl;
-
+  // std::cout << prg << std::endl;
+  //
   // Close the file
   file.close();
   return 0;
